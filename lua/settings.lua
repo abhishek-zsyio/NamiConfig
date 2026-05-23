eb1744b0c694efb5cefe2a66c6e357f9155fc9fb
Saurabh <saurabh.jain@heady.io>
2026-05-23 19:09:11 +0530

    chore: update lazy.nvim plugins and add support for Go, database, and testing tools

--- lua/settings.lua
+++ lua/settings.lua
@@ -13,7 +13,7 @@
 return {
   -- ── UI & Aesthetics ────────────────────────────────────────────────────────
   ---@type Theme
-  theme = "rose-pine",
+  theme = "poimandres",
   background = "dark",
   hide_empty_tabline = true,
   show_indent_guides = true,
